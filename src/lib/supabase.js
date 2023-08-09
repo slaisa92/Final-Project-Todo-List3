b9b0b806dd0453eaafd8318e8f9d94b342db2b0b
Petko Kirilov <Slaisa1992@gmail.com>
2023-08-09 19:42:46 +0200

    first commit

---
+++ src/lib/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://polfnwftviyobbnqfsnn.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvbGZud2Z0dml5b2JibnFmc25uIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE1OTkzMzgsImV4cCI6MjAwNzE3NTMzOH0.GqNRe4mtiWcYmmnd8CGb1aKSO6i9UKcLYxbz4LIAxVQ";
+
+export default createClient(supabaseUrl, supabaseAnonKey);
